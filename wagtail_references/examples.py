e69d736e0d30aff81322e7cf0c4f3d1c2c2a61b3
Tom Clark <>
2019-04-16 15:21:52 +0100

    FIX Code style made flake8 compliant

--- wagtail_references/examples.py
+++ wagtail_references/examples.py
@@ -4,7 +4,7 @@
 # These examples were exported from Mendeley Desktop (then internal/nonpublic items anonymised).
 
 article1 = """@article{Clark2017a,
-author = {Clark, Thomas H. E. and Lueck, Rolf G. and Hay, Alex E. and Davey, Thomas and Stern, Peter and Horwitz, Rachel and Pearson, Nicola},
+author = {Clark, Thomas and Lueck, Rolf G. and Hay, Alex E. and Davey, Thomas and Stern, Peter and Horwitz, Rachel},
 journal = {Proc. of the 12th European Wave and Tidal Energy Conference},
 pages = {7},
 title = {{InSTREAM : Measurement , Characterisation and Simulation of Turbulence from Test Tank to Ocean .}},
@@ -13,7 +13,7 @@ year = {2017}
 """
 
 article2 = """@article{Shives2011,
-abstract = {ABSTRACT:This paper presents a combined blade element (BE), computational fluid dynamics (CFD) method for performance analysis and optimization of ducted turbines. The model is similar to standard blade element momentum theory, except that CFD replaces the momentum equation for determining the induction factors. This eliminates many assumptions used in applying the typical blade element momentum (BEM) theory to a turbine and provides much richer flow information. It is also required for ducted turbines, since there is no fundamental momentum theory model that includes the impact of the duct on the flow field. The simulations use an axi-symmetric domain, which is computationally efficient since only a thin wedge of the entire geometry needs to be represented. A simple algorithm was developed to determine the optimum rotor loading and tip speed ratio for a given duct geometry within a user-defined level of granularity. This paper also demonstrates that for certain ducts, a non-uniform loading over the disk can improve overall performance by limiting flow separation within the duct. INTRODUCTIONDiffuser augmentation of wind turbines has been studied for decades by academics and companies such as Vortec (NZ) but with no commercially successful designs. This is thought to be due in part to the immense loading on the duct in storm conditions. With an emerging potential for tidal power generation, there is renewed interest in diffuser augmented turbines since tidal flows are less subject to extreme velocities. For example, Ireland''s OpenHydro has conducted tests with a high-solidity ducted turbine in the Bay of Fundy. Alstom (France) is developing a ducted turbine based on a design by Clean Current (Canada) also to be tested in the Bay of Fundy in 2012. Lunar energy (Scotland) and several other companies are also developing similar ducted designs.},
+abstract = {ABSTRACT:This paper presents a blah blah blah... },
 author = {Shives, Michael and Crawford, Curran},
 file = {:Users/me/Shives, Crawford - 2011 - Ducted Turbine Blade Optimization Using Numerical Simulation.pdf:pdf},
 isbn = {9781880653968},
@@ -47,13 +47,13 @@ title = {{Data Exchange Cover Sheet}}
 
 techreport2 = """@techreport{KS9b,
 author = {Clark, T.H. and Black, K. and Ibrahim, J. and Minns, N. and Fisher, S. and Roc, T. and Hernon, J. and White, R.},
-title = {{MRCF-TiME-KS9b Turbulence: Best practices for data processing, classification and characterisation of turbulent flows. A guide for the tidal power industry.}},
+title = {{MRCF-TiME-KS9b Turbulence: Best practices for data processing, classification and blah.}},
 year = {2015}
 }
 """
 
 phdthesis1 = """@phdthesis{robinson1990,
-abstract = {The long history of research into the internal structure of turbulent boundary layers has not provided a unified picture of the physics responsible for turbulence production and dissipation. The goals of the present research are to (1) define the current state of boundary layer structure knowledge and (2) utilize direct numerical simulation results to help close the unresolved issues identified in part A and to unify the fragmented knowledge of various coherent motions into a consistent kinematic model of boundary layer structure. The results of the current study show that all classes of coherent motion in the low Reynolds number turbulent boundary layer may be related to vortical structures, but that no single form of vortex is representative of the wide variety of vortical structures observed. In particular, ejection and sweep motions, as well as entrainment from the free-streem are shown to have strong spatial and temporal relationships with vortical structures. Disturbances of vortex size, location, and intensity show that quasi-streamwise vortices dominate the buffer region, while transverse vortices and vortical arches dominate the wake region. Both types of vortical structure are common in the log region. The interrelationships between the various structures and the population distributions of vortices are combined into a conceptual kinematic model for the boundary layer. Aspects of vortical structure dynamics are also postulated, based on time-sequence animations of the numerically simulated flow.},
+abstract = {The long history of research into the internal structure of turbulent boundary layers has not blah.},
 author = {Robinson, S K},
 file = {:Volumes/Robinson - 1991 - The kinematics of turbulent boundary layer structures.pdf:pdf},
 keywords = {coherent structures,kinematics,turbulent boundary layer},
@@ -66,11 +66,11 @@ year = {1991}
 """
 
 phdthesis2 = """@phdthesis{worthphd,
-abstract = {Further understanding the small scale coherent structures which occur in high Reynolds number turbulence would be of enormous benefit. Therefore, the aim of the current project was to make well resolved three-dimensional flow measurements of the mixing flow between counter rotating impellers, using Tomographic Particle Image Velocimetry (TPIV).
+abstract = {Further understanding blah blah...
 
-TPIV software was developed, with a novel approach permitting a significant reduction in processing time, and a series of numerical accuracy studies contributing to the fundamental understanding of this new technique. Basic flow characterisation determined the local isotropy, homogeneity and expected Reynolds number scaling. A favourable comparison between planar PIV and TPIV increased confidence in the latter, which was used to assess the dynamics and topology of the dissipation scale structures.
+blah TPIV software was developed, with a novel approach permitting a significant reduction in blah and
 
-In support of previous investigations similar topology, strain rate alignment, scale-invariance, and clustering behaviours are demonstrated. Correlated high enstrophy and dissipation regions occur in the periphery of larger structures, resulting in intermittency. Geometry characterisation indicates a predominance of tube-like structures, which are observed to form from larger ribbon-like structures through unsteady breakdown and vortex roll-up. Significant correlation between intermittent fields of dissipation and enstrophy describe the fine scales effects. These relationships should pave the way for more accurate models, capable of relating small scales and large scales during the prediction of dynamically important quantities.},
+In support of previous investigations similar topology, strain rate alignment, scale-invariance, and blah.},
 author = {Worth, Nicholas},
 keywords = {Coherent structures,Experimental,Fluid mechanics,Tomographic particle image velocimetry},
 mendeley-groups = {Tomographic PIV},
